9f6cbeb76c18734fb45935f2d1a60f73ca6aeb00
chenj <chenjie@_alan163.com>
2020-04-15 22:01:30 +0800

    first commit

---
+++ Java-Improve/src/main/java/com/chenj/JavaImprove/JavaImproveApplication.java
@@ -0,0 +1,13 @@
+package com.chenj.JavaImprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaImproveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaImproveApplication.class, args);
+	}
+
+}
